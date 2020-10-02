3c2051b5f0d955b99001ab854022185827d91839
Andrey Masterenko J <amasterenko@gmail.com>
2020-10-02 14:09:36 +0300

    5. Очередь на двух стеках [#365936]: -edited: method isEmpty() was added in ForwardLinked and SimpleStack clases, SimpleQueue's code was optimized.

--- chapter_001/src/main/java/ru/job4j/collection/ForwardLinked.java
+++ chapter_001/src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -41,6 +41,10 @@ public class ForwardLinked<T> implements Iterable<T> {
         return rsl;
     }
 
+    public boolean isEmpty() {
+        return head == null;
+    }
+
     @Override
     public Iterator<T> iterator() {
         return new Iterator<T>() {
--- chapter_001/src/main/java/ru/job4j/collection/SimpleQueue.java
+++ chapter_001/src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -5,26 +5,20 @@ import java.util.NoSuchElementException;
 public class SimpleQueue<T> {
     private final SimpleStack<T> in = new SimpleStack<>();
     private final SimpleStack<T> out = new SimpleStack<>();
-    private int inCounter = 0;
-    private int outCounter = 0;
 
     public T poll() {
-        if (inCounter == 0 && outCounter == 0) {
+        if (in.isEmpty() && out.isEmpty()) {
             throw new NoSuchElementException("Queue is empty!");
         }
-        if (outCounter == 0) {
-            while (inCounter > 0) {
+        if (out.isEmpty()) {
+            while (!in.isEmpty()) {
                 out.push(in.pop());
-                outCounter++;
-                inCounter--;
             }
         }
-        outCounter--;
         return out.pop();
     }
 
     public void push(T value) {
         in.push(value);
-        inCounter++;
     }
 }
