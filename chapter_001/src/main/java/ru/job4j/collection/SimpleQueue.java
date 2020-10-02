e51c7d7c48933eec292c159917b527d33124c5f0
Andrey Masterenko J <amasterenko@gmail.com>
2020-10-02 13:53:28 +0300

    5. Очередь на двух стеках [#365936]: edited - optimized code

--- chapter_001/src/main/java/ru/job4j/collection/SimpleQueue.java
+++ chapter_001/src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -5,28 +5,25 @@ import java.util.NoSuchElementException;
 public class SimpleQueue<T> {
     private final SimpleStack<T> in = new SimpleStack<>();
     private final SimpleStack<T> out = new SimpleStack<>();
-    int inCounter = 0;
-    int outCounter = 0;
+    private int inCounter = 0;
+    private int outCounter = 0;
 
     public T poll() {
-        if (inCounter == 0) {
+        if (inCounter == 0 && outCounter == 0) {
             throw new NoSuchElementException("Queue is empty!");
         }
-        while (inCounter > 1) {
-            out.push(in.pop());
-            outCounter++;
-            inCounter--;
+        if (outCounter == 0) {
+            while (inCounter > 0) {
+                out.push(in.pop());
+                outCounter++;
+                inCounter--;
+            }
         }
-        inCounter--;
-        return in.pop();
+        outCounter--;
+        return out.pop();
     }
 
     public void push(T value) {
-        while (outCounter > 0) {
-            in.push(out.pop());
-            outCounter--;
-            inCounter++;
-        }
         in.push(value);
         inCounter++;
     }
