e1f3956ca3fd36821306114d91432fb20f95a88b
Andrey Masterenko H <masterenko.as@gmail.com>
2020-10-22 22:35:46 +0300

    0.1. FileOutputStream. [#365982]

---
+++ chapter_002/src/main/java/ru/job4j/io/MultiplicationTable.java
@@ -0,0 +1,28 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        try (FileOutputStream fileOut = new FileOutputStream("multiplication_table.txt")) {
+            StringBuilder sOut = new StringBuilder("   ");
+            for (int i = 1; i <= 10; i++) {
+                sOut.append(i).append("  ");
+            }
+            sOut.append("\n");
+            fileOut.write(sOut.toString().getBytes());
+            sOut.setLength(0);
+            for (int i = 1; i <= 10; i++) {
+                sOut.append(i).append(i != 10 ? "  " : " ");
+                for (int j = 1; j <= 10; j++) {
+                    sOut.append(i * j).append(i * j >= 10 ? " " : "  ");
+                }
+                sOut.append("\n");
+                fileOut.write(sOut.toString().getBytes());
+                sOut.setLength(0);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
