3689233a865c29a67980a31ed139be019428c461
Andrey Masterenko H <masterenko.as@gmail.com>
2020-10-23 07:56:14 +0300

    0.1. FileOutputStream. [#365982]

---
+++ chapter_002/src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -0,0 +1,22 @@
+package ru.job4j.io;
+
+import java.io.FileInputStream;
+
+public class EvenNumberFile {
+    public static void main(String[] args) {
+        try (FileInputStream in = new FileInputStream("even.txt")) {
+            StringBuilder text = new StringBuilder();
+            int read;
+            while ((read = in.read()) != -1) {
+                text.append((char) read);
+            }
+            String[] numbers = text.toString().split(System.lineSeparator());
+            for (String num: numbers) {
+                System.out.println("Number " + num + " is "
+                        + (Integer.parseInt(num) % 2 == 0 ? "even" : "odd"));
+            }
+                    } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
