e8bf3f8f3f9eef77d9ddec198d9ba3bff27c7ceb
Andrey Masterenko H <masterenko.as@gmail.com>
2020-10-25 14:58:57 +0300

    3.0. Тестирование IO [#365980]

--- chapter_002/src/test/java/ru/job4j/io/AnalizeTest.java
+++ chapter_002/src/test/java/ru/job4j/io/AnalizeTest.java
@@ -1,64 +1,86 @@
 package ru.job4j.io;
 
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.junit.Rule;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.io.*;
 
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class AnalizeTest {
-    private final String targetFile = "target.log";
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
 
     @Test
-    public void whenSourceHas200And300TypesThenTargetIsEmpty() {
-        Analize analize = new Analize();
-        analize.unavailable("./data/source200And300.log", targetFile);
-        List<String> result = null;
-        try (BufferedReader reader = new BufferedReader(new FileReader(targetFile))) {
-            result = reader.lines()
-                    .collect(Collectors.toList());
-        } catch (Exception e) {
-            e.printStackTrace();
+    public void whenSourceHas200And300TypesThenTargetIsEmpty() throws IOException {
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.txt");
+        try (PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(source)))) {
+            writer.println("200 10:56:01");
+            writer.println("200 10:57:01");
+            writer.println("300 10:58:01");
+            writer.println("200 10:59:01");
+            writer.println("300 11:01:02");
+            writer.println("200 11:02:02");
         }
-        assertThat(result, is(Collections.EMPTY_LIST));
+        Analize analize = new Analize();
+        analize.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder result = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(target))) {
+            reader.lines().forEach(result::append);
+        }
+        assertThat(result.toString(), is(""));
     }
 
     @Test
-    public void whenSourceHas400And500TypesThenTargetHasOneLine() {
-        Analize analize = new Analize();
-        analize.unavailable("./data/source400And500.log", targetFile);
-        List<String> result = null;
-        List<String> expected = Arrays.asList("10:56:01;11:02:02");
-        try (BufferedReader reader = new BufferedReader(new FileReader(targetFile))) {
-            result = reader.lines()
-                    .collect(Collectors.toList());
-        } catch (Exception e) {
-            e.printStackTrace();
+    public void whenSourceHas400And500TypesThenTargetHasOneLine() throws IOException {
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.txt");
+        try (PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(source)))) {
+            writer.println("400 10:56:01");
+            writer.println("400 10:57:01");
+            writer.println("500 10:58:01");
+            writer.println("400 10:59:01");
+            writer.println("400 11:01:02");
+            writer.println("500 11:02:02");
         }
-        assertThat(result, is(expected));
+        String expected = "10:56:01;11:02:02";
+        Analize analize = new Analize();
+        analize.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder result = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(target))) {
+            reader.lines().forEach(result::append);
+        }
+        assertThat(result.toString(), is(expected));
     }
 
     @Test
-    public void whenSourceHasAllTypesThenTarget3Lines() {
-        Analize analize = new Analize();
-        analize.unavailable("./data/sourceAllTypes.log", targetFile);
-        List<String> result = null;
-        List<String> expected = Arrays.asList(
-                "10:57:01;10:59:01",
-                "11:02:02;11:03:02",
-                "11:04:02;11:05:02");
-        try (BufferedReader reader = new BufferedReader(new FileReader(targetFile))) {
-            result = reader.lines()
-                    .collect(Collectors.toList());
-        } catch (Exception e) {
-            e.printStackTrace();
+    public void whenSourceHasAllTypesThenTarget3Lines() throws IOException {
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.txt");
+        try (PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(source)))) {
+            writer.println("200 10:56:01");
+            writer.println("400 10:57:01");
+            writer.println("500 10:58:01");
+            writer.println("300 10:59:01");
+            writer.println();
+            writer.println("200 11:01:02");
+            writer.println("400 11:02:02");
+            writer.println("300 11:03:02");
+            writer.println("500 11:04:02");
+            writer.println("200 11:05:02");
         }
-        assertThat(result, is(expected));
+        String expected = "10:57:01;10:59:01"
+                + "11:02:02;11:03:02"
+                + "11:04:02;11:05:02";
+        Analize analize = new Analize();
+        analize.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder result = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(target))) {
+            reader.lines().forEach(result::append);
+        }
+        assertThat(result.toString(), is(expected));
     }
 }
