5be6a27e6513106bf820c5aa1e9a6cf744cde8f1
Andrey Masterenko H <masterenko.as@gmail.com>
2020-10-26 08:29:07 +0300

    5.1. Именованные аргументы [#365986]

---
+++ chapter_002/src/main/java/ru/job4j/io/ArgsName.java
@@ -0,0 +1,39 @@
+package ru.job4j.io;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArgsName {
+
+    private final Map<String, String> values = new HashMap<>();
+
+    public String get(String key) {
+        return values.get(key);
+    }
+
+    private void parse(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Missing parameters: -param1=value1 ...-paramN=valueN");
+        }
+        for (String a : args) {
+            if (a.charAt(0) != '-' || !a.contains("=")) {
+                throw new IllegalArgumentException("Invalid parameters. Valid format: -paramN=valueN");
+            }
+            values.put(a.split("=")[0].substring(1), a.split("=")[1]);
+        }
+    }
+
+    public static ArgsName of(String[] args) {
+        ArgsName names = new ArgsName();
+        names.parse(args);
+        return names;
+    }
+
+    public static void main(String[] args) {
+        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8"});
+        System.out.println(jvm.get("Xmx"));
+
+        ArgsName zip = ArgsName.of(new String[] {"-out=project.zip", "-encoding=UTF-8"});
+        System.out.println(zip.get("out"));
+    }
+}
