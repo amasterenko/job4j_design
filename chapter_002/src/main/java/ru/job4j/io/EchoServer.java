825ee8a4fe7f1645a1b12e14f8068292ba617709
Andrey Masterenko H <masterenko.as@gmail.com>
2020-10-29 21:28:07 +0300

    4. Slf4j - вывод exception. [#365977]

--- chapter_002/src/main/java/ru/job4j/io/EchoServer.java
+++ chapter_002/src/main/java/ru/job4j/io/EchoServer.java
@@ -3,38 +3,46 @@ package ru.job4j.io;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class EchoServer {
-    public static void main(String[] args) throws IOException {
-        try (ServerSocket server = new ServerSocket(9000)) {
-            while (true) {
-                Socket socket = server.accept();
-                try (OutputStream out = socket.getOutputStream();
-                     BufferedReader in = new BufferedReader(
-                             new InputStreamReader(socket.getInputStream()))) {
-                    boolean isStop = false;
-                    String answer = "";
-                    String str;
-                    while (!(str = in.readLine()).isEmpty()) {
-                        System.out.println(str);
-                        if (str.toLowerCase().contains("msg=")) {
-                            if (str.toLowerCase().contains("exit")) {
-                                isStop = true;
-                            }
-                            if (str.toLowerCase().contains("hello")) {
-                                answer = "Hello, dear friend!";
-                            } else {
-                                answer = str.split("=")[1].split(" ")[0];
+    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
+
+    public static void main(String[] args) {
+        try {
+            try (ServerSocket server = new ServerSocket(9000)) {
+                while (true) {
+                    Socket socket = server.accept();
+                    try (OutputStream out = socket.getOutputStream();
+                         BufferedReader in = new BufferedReader(
+                                 new InputStreamReader(socket.getInputStream()))) {
+                        boolean isStop = false;
+                        String answer = "";
+                        String str;
+                        while (!(str = in.readLine()).isEmpty()) {
+                            System.out.println(str);
+                            if (str.toLowerCase().contains("msg=")) {
+                                if (str.toLowerCase().contains("exit")) {
+                                    isStop = true;
+                                }
+                                if (str.toLowerCase().contains("hello")) {
+                                    answer = "Hello, dear friend!";
+                                } else {
+                                    answer = str.split("=")[1].split(" ")[0];
+                                }
                             }
                         }
+                        if (isStop) {
+                            break;
+                        }
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        out.write(answer.getBytes());
                     }
-                    if (isStop) {
-                        break;
-                    }
-                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    out.write(answer.getBytes());
                 }
             }
+        } catch (IOException e) {
+            LOG.error("An exception has occurred: ", e);
         }
     }
 }
