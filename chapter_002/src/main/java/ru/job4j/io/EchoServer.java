6f93d6b8547ee3a595956c7f883ca36d031eb489
Andrey Masterenko H <masterenko.as@gmail.com>
2020-12-21 22:09:02 +0300

    EchoServer edited

--- chapter_002/src/main/java/ru/job4j/io/EchoServer.java
+++ chapter_002/src/main/java/ru/job4j/io/EchoServer.java
@@ -1,10 +1,14 @@
 package ru.job4j.io;
 
-import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class EchoServer {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
@@ -19,8 +23,8 @@ public class EchoServer {
                                  new InputStreamReader(socket.getInputStream()))) {
                         boolean isStop = false;
                         String answer = "";
-                        String str;
-                        while (!(str = in.readLine()).isEmpty()) {
+                        String str = in.readLine();
+                        while (!str.isEmpty()) {
                             System.out.println(str);
                             if (str.toLowerCase().contains("msg=")) {
                                 if (str.toLowerCase().contains("exit")) {
@@ -32,6 +36,7 @@ public class EchoServer {
                                     answer = str.split("=")[1].split(" ")[0];
                                 }
                             }
+                            str = in.readLine();
                         }
                         if (isStop) {
                             break;
