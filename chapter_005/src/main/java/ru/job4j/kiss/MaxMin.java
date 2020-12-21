25146c0a4f43d894c588b7e84164637d8a55e31d
Andrey Masterenko J <amasterenko@gmail.com>
2020-12-21 16:05:44 +0300

    1. Принципы Kiss, Dry и Yagni [#366008]

---
+++ chapter_005/src/main/java/ru/job4j/kiss/MaxMin.java
@@ -0,0 +1,24 @@
+package ru.job4j.kiss;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class MaxMin {
+    public <T> T max(List<T> value, Comparator<T> comparator) {
+        return compareListElements(value, comparator);
+    }
+
+    public <T> T min(List<T> value, Comparator<T> comparator) {
+        return compareListElements(value, comparator.reversed());
+    }
+
+    private <T> T compareListElements(List<T> list, Comparator<T> comparator) {
+        T extremum = list.get(0);
+        for (int i = 1; i < list.size(); i++) {
+            if (comparator.compare(extremum, list.get(i)) < 0) {
+                extremum = list.get(i);
+            }
+        }
+        return extremum;
+    }
+}
---
+++ chapter_005/src/test/java/ru/job4j/kiss/MaxMinTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.kiss;
+
+import org.junit.Test;
+
+import java.util.Comparator;
+import java.util.List;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class MaxMinTest {
+
+    @Test
+    public void whenIntMax() {
+        List<Integer> list = List.of(2, 5, 10, -1, 0, 3);
+        MaxMin mm = new MaxMin();
+        Comparator<Integer> comp = Comparator.comparingInt(el -> el);
+        assertThat(mm.max(list, comp), is(10));
+    }
+
+    @Test
+    public void whenIntMin() {
+        List<Integer> list = List.of(2, 5, 10, -1, 0, 3);
+        MaxMin mm = new MaxMin();
+        Comparator<Integer> comp = Comparator.comparingInt(el -> el);
+        assertThat(mm.min(list, comp), is(-1));
+    }
+
+}
